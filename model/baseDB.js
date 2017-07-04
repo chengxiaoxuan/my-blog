efe29cb521308bab61cafe42a5ceb7283f8ab89d
cxx <965410379@qq.com>
2017-07-04 21:03:58 +0800

    修改数据库

--- model/baseDB.js
+++ model/baseDB.js
@@ -1,8 +1,5 @@
 var Sequelize = require('sequelize');
 
-var connection = new Sequelize('my-blog', 'root', '123456', {
-    host: '127.0.0.1',
-    dialect: 'mysql'
-});
+
 
 module.exports = connection;
